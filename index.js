b0d0c77c40e35012bfdd4f0aace2918fb06c401f
Empezeeet <pawelkomateusz@gmail.com>
2025-05-15 11:36:52 +0200

    init

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("aaaa");
+let canvas = document.querySelector("#canvas1");
+let ctx = canvas.getContext("2d");
+// 128x128 map
+const difficulty = 80;
+let map = [];
+for (let y=0; y<20; y++) {
+    map[y] = [];
+    for (let x=0; x<20; x++) {
+        map[y][x] = Math.round(Math.random()*100)-1;
+        ctx.beginPath();
+        ctx.rect(32*x, 32*y, 32,32);
+        ctx.fillStyle =  map[y][x]<difficulty ? "green" : "red";
+        ctx.fill();        
+    }
+}
+function getMousePosition(canvas, event) {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    console.log("Coordinate x: " + x,
+        "Coordinate y: " + y);
+    let mapX = Math.floor(x/32);
+    let mapY = Math.floor(y/32);
+    console.log(map[mapY][mapX]<difficulty ? "green" : "red");
+}
+canvas.addEventListener("mousedown", (e) => {
+    getMousePosition(canvas, e);
+})
