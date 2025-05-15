a1d21461b116cef4aa8208fa4281a03c00c3e0de
Mateusz <pawelkomateusz@gmail.com>
2025-05-15 18:01:43 +0200

    first move is never bomb

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ console.log("aaaa");
 let canvas = document.querySelector("#canvas1");
 let ctx = canvas.getContext("2d");
 // 128x128 map
-const difficulty = 80;
+const difficulty = 60;
+let firstMove = true;
 let map = [];
 for (let y=0; y<20; y++) {
     map[y] = [];
@@ -14,6 +15,16 @@ for (let y=0; y<20; y++) {
         ctx.fill();        
     }
 }
+function refresh() {
+    for (let y=0; y<20; y++) {
+        for (let x=0; x<20; x++) {
+            ctx.beginPath();
+            ctx.rect(32*x, 32*y, 32,32);
+            ctx.fillStyle =  map[y][x]<difficulty ? "green" : "red";
+            ctx.fill();        
+        }
+    }
+}
 function getMousePosition(canvas, event) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
@@ -22,8 +33,15 @@ function getMousePosition(canvas, event) {
         "Coordinate y: " + y);
     let mapX = Math.floor(x/32);
     let mapY = Math.floor(y/32);
+    if (firstMove) {
+        map[mapY][mapX] = 0;
+        firstMove = false;
+    }
+
+    
     console.log(map[mapY][mapX]<difficulty ? "green" : "red");
 }
 canvas.addEventListener("mousedown", (e) => {
     getMousePosition(canvas, e);
+    refresh();
 })
